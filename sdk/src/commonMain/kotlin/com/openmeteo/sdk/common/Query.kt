089e80c5d2f1e9bc7453b718700f2d31343017db
DadiBit <38182988+DadiBit@users.noreply.github.com>
2024-01-21 18:07:06 +0100

    feat: Query interfaces
    Removed all docs, good idea to steal them back from original source code when done fixing up this mess...

---
+++ sdk/src/commonMain/kotlin/com/openmeteo/sdk/common/Query.kt
@@ -0,0 +1,89 @@
+package com.openmeteo.sdk.common
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.serialization.Serializable
+
+interface Query {
+
+    interface Coordinates {
+        val latitude: QueryList<Float>
+        val longitude: QueryList<Float>
+    }
+
+    interface Elevations {
+        val elevation: QueryList<Float>?
+    }
+
+    interface TimeZones {
+        val timezone: QueryList<TimeZone>?
+    }
+
+    interface DateFrames {
+        val start_date: QueryList<LocalDate>?
+        val end_date: QueryList<LocalDate>?
+    }
+
+    interface HourFrames {
+        val start_hour: QueryList<LocalDateTime>?
+        val end_hour: QueryList<LocalDateTime>?
+    }
+
+    interface Minutely15Frames {
+        val start_minutely_15: QueryList<LocalDateTime>?
+        val end_minutely_15: QueryList<LocalDateTime>?
+    }
+
+    interface TimeFormat
+
+    interface Daily<V> : TimeFormat {
+        val daily: QueryList<V>
+    }
+
+    interface Hourly<V> : TimeFormat {
+        val hourly: QueryList<V>
+    }
+
+    interface Minutely15<V> : TimeFormat {
+        val minutely_15: QueryList<V>
+    }
+
+    interface CurrentWeather<V> : TimeFormat {
+        val current: QueryList<V>
+    }
+
+    interface PastDays {
+        val past_days: Int?
+    }
+
+    interface ForecastDays {
+        val forecast_days: Int?
+    }
+
+    interface PastHours {
+        val past_hours: Int?
+    }
+
+    interface ForecastHours {
+        val forecast_hours: Int?
+    }
+
+    interface PastMinutely15 {
+        val past_minutely_15: Int?
+    }
+
+    interface ForecastMinutely15 {
+        val forecast_minutely_15: Int?
+    }
+
+    interface ContentFormat {
+        val format: com.openmeteo.sdk.common.ContentFormat?
+    }
+
+    interface CommercialLicense {
+        val apikey: String?
+    }
+
+}
